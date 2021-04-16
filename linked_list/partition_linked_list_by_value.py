5da2ee4eaf0abfecde0cb1503df564d38a49f923
Sowmen Rahman <sowmen.appd@gmail.com>
2021-04-16 20:22:57 +0600

    Added stable partition algorithm

--- linked_list/partition_linked_list_by_value.py
+++ linked_list/partition_linked_list_by_value.py
@@ -1,9 +1,9 @@
 from linked_list.core.linked_list import LinkedList
 
 # in this approach two lists are created
-# using an additional space of O(n)
+# using an additional memory of O(n)
 # O(n) - time
-# O(n) - space
+# O(n) - memory
 def partition_linked_list_by_value(ll, val):
     before = LinkedList()
     after = LinkedList()
@@ -32,9 +32,9 @@ def partition_linked_list_by_value(ll, val):
 # we add nodes to the front if less 
 # and at the end if equal or more
 # O(n) - time
-# O(1) - space
+# O(1) - memory
 
-def partition_linked_list_by_value_optimized_unordered(ll, val):
+def partition_linked_list_by_value_optimized_unstable(ll, val):
     head = ll.head
     tail = ll.head
 
@@ -54,4 +54,45 @@ def partition_linked_list_by_value_optimized_unordered(ll, val):
     
     ll.head = head
 
+    return ll
+
+
+# in this approach we use 4 variables 
+# to keep track of the nodes added 
+# before or after, the list is stably 
+# organised
+# O(n) - time
+# O(1) - memory
+
+def partition_linked_list_by_value_optimized_stable(ll, val):
+    beforeHead = None
+    beforeTail = beforeHead
+    
+    afterHead = None
+    afterTail = afterHead
+
+    current = ll.head
+
+    while current:
+        next = current.next
+
+        if current.data < val:
+            if not beforeHead:
+                beforeHead, beforeTail = current, current
+            else:
+                beforeTail.next = current
+                beforeTail = current
+        else:
+            if not afterHead:
+                afterHead, afterTail = current, current
+            else:
+                afterTail.next = current
+                afterTail = current
+        current = next
+
+    afterTail.next = None
+    
+    beforeTail.next = afterHead
+
+    ll.head = beforeHead
     return ll
