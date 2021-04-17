d12ed21adf51fdb1da8152ea085fbb3d16604c7d
Sowmen Rahman <sowmen.appd@gmail.com>
2021-04-17 17:29:24 +0600

    Stack modified to support min function

--- stack/core/stack.py
+++ stack/core/stack.py
@@ -1,20 +1,44 @@
+import math
+
 class Stack:
     def __init__(self):
         self.index = -1
         self.stack = []
+        self.minItems = []
     
     def push(self, val):
         self.index += 1
         self.stack.append(val)
-
+        if len(self.minItems) > 0:
+            if val < self.minItems[-1]:
+                self.minItems.append(val)
+        else:
+            self.minItems.append(val)
     def pop(self):
-        if index < 0:
+        if self.empty():
             raise IndexError()
+
+        if len(self.minItems) > 0:
+            if self.minItems[-1] == self.stack[-1]:
+                self.minItems.pop()
+        
         self.stack.pop()
         self.index -= 1
     
+    def empty(self):
+        return self.index < 0
+
     def peek(self):
         if self.index > -1:
             return self.stack[self.index]
         else:
             return None
+
+    def min(self):
+        if len(self.minItems) > 0:
+            return self.minItems[-1]
+        else:
+            return None
+
+    def print(self):
+        print(self.stack)
